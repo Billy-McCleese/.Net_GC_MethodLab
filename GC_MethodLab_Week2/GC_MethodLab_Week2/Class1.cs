7864e7cff6beb243a908a625b09f75fa388535bb
Billy-McCleese <billymccleese@gmail.com>
2023-01-22 14:54:18 -0500

    Billy McCleese Method Lab submission

---
+++ GC_MethodLab_Week2/GC_MethodLab_Week2/Class1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceMethodLab
+
+{
+    public static class GetDiceRoll
+    {
+       static Random rand = new Random(); // only make one for dice example
+        public static int DiceRoll()
+        {
+
+                int myrandomnum1 = rand.Next(1, 7);
+                return myrandomnum1;        
+            
+        }
+    }
+    public static class GetDiceComboName
+    {
+        static string? DieResultName;
+        public static string DiceComboName(int Die1Num, int Die2Num)
+        {
+            if (Die1Num == 1 && Die2Num == 1)
+            {
+                DieResultName = "Snake Eyes";
+                return DieResultName;
+            }
+            else if ((Die1Num == 1 && Die2Num== 2) || (Die1Num == 2 && Die2Num == 1))
+                    {
+                DieResultName = "Ace Duece";
+                return DieResultName;
+            }
+            else if (Die1Num == 6 && Die2Num == 6)
+            {
+                DieResultName = "Box Cars";
+                return DieResultName;
+            }
+
+            return string.Empty;
+           
+        }
+    }
+    public static class GetDiceTotalName
+    {
+        static string? DieResultTotalName;
+        public static string DiceTotalName(int Die1Num, int Die2Num)
+        {
+            if (Die1Num + Die2Num == 7 || Die1Num + Die2Num == 11)
+            {
+                DieResultTotalName = "Win";
+                return DieResultTotalName;
+            }
+            else if (Die1Num + Die2Num == 2 || Die1Num + Die2Num == 3 || Die1Num + Die2Num == 12)
+            {
+                DieResultTotalName = "Craps";
+                return DieResultTotalName;
+            }
+            return string.Empty;
+
+        }
+    }
+} 
---
+++ GC_MethodLab_Week2/GC_MethodLab_Week2/Program.cs
@@ -0,0 +1,51 @@
+﻿using DiceMethodLab;
+bool Close = false;
+
+Console.WriteLine("Welcome To Billy's Casino, where we only play this one game. Hope that's what you're into.\r\nWould you like to Roll? (y/n)");
+String? UserRollInput = (Console.ReadLine());
+
+if (UserRollInput is "Y")
+{
+    Close = false;
+}
+else if (UserRollInput is "y")
+{ 
+    Close = false;
+
+}
+else
+{
+    Close = true;
+    Console.WriteLine("Thanks for Playing! Come again!");
+}
+
+while (!Close)
+{
+    int Die1Result = GetDiceRoll.DiceRoll();
+    int Die2Result = GetDiceRoll.DiceRoll();
+    int DieTotal = (Die1Result + Die2Result);
+    string DiceComboNameResult = GetDiceComboName.DiceComboName(Die2Result, Die1Result);
+    string DiceTotalNameResult = GetDiceTotalName.DiceTotalName(Die1Result, Die2Result);
+
+
+    Console.WriteLine("You rolled a " + Die1Result + " and a " + Die2Result + " (" + DieTotal + " Total)");
+    Console.WriteLine(DiceComboNameResult);
+    Console.WriteLine(DiceTotalNameResult);
+    Console.WriteLine("Would you like to Roll again? (y/n)");
+    UserRollInput = (Console.ReadLine());
+    if (UserRollInput is "Y")
+    {
+        Close = false;
+    }
+    else if (UserRollInput is "y")
+    {
+        Close = false;
+
+    }
+    else
+    {
+        Close = true;
+        Console.WriteLine("Thanks for Playing! Come again!");
+    }
+}
+
